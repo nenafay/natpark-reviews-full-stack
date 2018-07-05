f638f57f38b247cbfc4a7263a004eb8f1f245c59
Nena Park <nena.park@gmail.com>
2018-07-05 14:15:11 -0400

    initial commit, used spring initializr to build file structure

---
+++ src/main/java/reviews/fullstack/national/parks/NationalParksApplication.java
@@ -0,0 +1,12 @@
+package reviews.fullstack.national.parks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalParksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalParksApplication.class, args);
+	}
+}
---
+++ src/test/java/reviews/fullstack/national/parks/NationalParksApplicationTests.java
@@ -0,0 +1,16 @@
+package reviews.fullstack.national.parks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NationalParksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
