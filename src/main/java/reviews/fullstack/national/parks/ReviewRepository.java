9eb82dc044db86b11e99c8e45725f847d0fce2be
Nena Park <nena.park@gmail.com>
2018-07-06 16:04:43 -0400

    fifth commit - 3/7 thests pass. Way less red. Added tag class and tag repo interface

--- src/main/java/reviews/fullstack/national/parks/ReviewRepository.java
+++ src/main/java/reviews/fullstack/national/parks/ReviewRepository.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReviewRepository extends CrudRepository<Review, Long> {
-	Collection<Review> findByTripContains(Trip trip);
+	Collection<Review> findByTrip(Trip trip);
 	
 	Collection<Review> findByTripId(Long tripId);
 }
--- src/test/java/reviews/fullstack/national/parks/JPAMappingTest.java
+++ src/test/java/reviews/fullstack/national/parks/JPAMappingTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertThat;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import javax.annotation.Resource;
@@ -28,6 +29,9 @@ public class JPAMappingTest {
 	
 	@Resource
 	private TripRepository tripRepo;
+	
+	@Resource
+	private TagRepository tagRepo;
 	
 	@Test
 	public void shouldSaveAndLoadReview() {
@@ -92,6 +96,56 @@ public class JPAMappingTest {
 		
 	@Test
 	public void shouldFindReviewsForTrip() {
+		Trip aug2015 = tripRepo.save(new Trip("August, 2015", "Janna's Wedding"));
+		
+		Review adirondack = reviewRepo.save(new Review ("Adirondack National Park", "lots of mosquitoes", aug2015));
+		Review acadia = reviewRepo.save(new Review ("Acadia National Park", "no moose spotted", aug2015));
+		
+		entityManager.flush();
+		entityManager.clear();
+		
+		Collection<Review>reviewsForTrip = reviewRepo.findByTrip(aug2015);
+		
+		assertThat(reviewsForTrip, containsInAnyOrder(adirondack, acadia));	
+	}
+	
+	@Test
+	public void shouldFindReviewsForTripId() {
+		Trip aug2015 = tripRepo.save(new Trip("August, 2015", "Janna's Wedding"));
+		long tripId = aug2015.getId();
+		
+		Review adirondack = reviewRepo.save(new Review ("Adirondack National Park", "lots of mosquitoes", aug2015));
+		Review acadia = reviewRepo.save(new Review ("Acadia National Park", "no moose spotted", aug2015));
+		
+		entityManager.flush();
+		entityManager.clear();
+		
+		Collection<Review>reviewsForTrip = reviewRepo.findByTripId(tripId);
+		
+		assertThat(reviewsForTrip, containsInAnyOrder(acadia, adirondack));
+
+	}
+	
+	@Test
+	public void shouldEstablishTagToReviewRelationship() {
+		Trip aug2015 = tripRepo.save(new Trip("August, 2015", "Janna's Wedding"));
+		
+		Review acadia = reviewRepo.save(new Review ("Acadia National Park", "no moose spotted", aug2015));
+		reviewRepo.save(review);
+		long reviewId = review.getId();
+		
+		Tag amenity = new Tag("amenity", review);
+		tagRepo.save(amenity);
+		
+		Tag amenity2 = new Tag("amenity", review);
+		tagRepo.save(amenity2);
+		
+		entityManager.flush();
+		entityManager.clear();
+		
+		Optional<Review>result = reviewRepo.findById(reviewId);
+		review = result.get();
+		assertThat(review.getTags(), containsInAnyOrder(amenity, amenity2));
 		
 	}
 
